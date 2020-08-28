9684b69d990a411fb34a4bd50f4b888a39a864d6
WIDS-Ife <69414995+WIDS-Ife@users.noreply.github.com>
2020-08-28 19:11:34 +0100

    Delete Steps.py

--- Steps_to_follow/Steps.py
+++
@@ -1,9 +0,0 @@
-Steps to take before modeling (when dealing with a dataset)
-
-1. Exploratory data analysis
-
-2. Data cleaning and feature engineering
-
-3. Model selection and hyper-parameter tuning
-
-4. Predictive modelling
